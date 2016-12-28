f11b2384b06a45bcfa1edb899710aad5c99ac9d3
sasata299 <karaage299@gmail.com>
2016-12-29 01:48:10 +0900

    他にもwhereやrandomを試してみる

--- foobar.js
+++ foobar.js
@@ -9,3 +9,15 @@ _.each(members, function(member, i) {
   console.log(member);
   console.log(i);
 });
+
+var humans = [
+  { name:"Christ", religion:"Christianity", birth:1 },
+  { name:"Buddha", religion:"Buddhism", birth:-563 },
+  { name:"Bill Gates", religion:"Christianity", birth:1955 },
+  { name:"Steve Jobs", religion:"Buddhism", birth:1955 }
+];
+
+var results = _.where(humans, { religion: "Buddhism" });
+console.log(results);
+
+console.log(_.random(0, 100));
